9d1e9eaba848098a9940dfa26a5249576d70bb22
Vipransh <vipransh231527@gmail.com>
2025-09-29 02:04:56 +0530

    Started JavaScript and Day1 done

---
+++ javascript/day1Basics/index.js
@@ -0,0 +1,25 @@
+console.log("My name is Vipransh");
+console.log("Ik i am revising");
+alert("You know you have time bro i believe yes")
+var a = prompt("you there my Friend")
+
+console.log(a);
+
+
+// we have 7 primitive data typeof
+typeof(a);
+// Integer, String, Boolean, Undefined,
+// Null , BigInt , Symbol
+
+
+
+// Non Primitive
+// Objects - collections of values , Stores Key Value pair
+// we usually use const to declare an object
+
+const student = {
+    fullName : "Vipransh Anand",
+    marks : 23,
+    age : 21,
+    isPass : true
+}
