f74cb9a8203c39d29f42e534e52a7cfe6082656b
Vipransh <vipransh231527@gmail.com>
2025-09-30 00:28:49 +0530

    Day3 javascript revision complete, loops and strings

---
+++ javascript/day3loops/index.js
@@ -0,0 +1,15 @@
+console.log(1);
+console.log("Writing this inside the loop")
+
+for(let i = 0 ;i < 5; i++){
+    console.log(i);
+}
+
+console.log("Loop has ended");
+
+// we have 
+// simple for, 
+// while
+//  do while
+// for of loop 
+// for in loop
